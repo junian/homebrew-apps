44eab60f98ca4cb38443675bac01ed6df03e2e82
Junian Triajianto <author@junian.net>
2020-12-14 09:40:22 +0700

    Update Turbo Switch Pro v2.10.2

--- Casks/turbo-boost-switcher-pro.rb
+++ Casks/turbo-boost-switcher-pro.rb
@@ -1,15 +1,16 @@
 cask "turbo-boost-switcher-pro" do
-  version "2.10.1"
-  sha256 "9c74e2965432fe25c804e99def7f211b3cd35421fd2b261bd9011edc59d05a84"
+  version "2.10.2"
+  sha256 "32ca4284914a6a69cd7dac0d8641a270323ba073a21be0ced044bb748691a9e4"
 
-  # turbo-boost-switcher.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://files.gumroad.com/attachments/2401211130126/1efdeb8808c5476391e2a74457130dfe/original/Turbo_Boost_Switcher_Pro_v#{version}.dmg?response-content-disposition=attachment&verify=#{ENV['HOMEBREW_GUMROADVERIFY']}"
+  # files.gumroad.com/ was verified as official when first introduced to the cask
+  url "https://files.gumroad.com/attachments/2401211130126/b5d96270c67544619bc6ccd1d5145142/original/Turbo_Boost_Switcher_Pro_v2.10.2.dmg?response-content-disposition=attachment&verify=#{ENV['HOMEBREW_GUMROADVERIFY']}"
+      verified: "files.gumroad.com/"
   appcast "https://www.rugarciap.com/turbo-boost-switcher-for-os-x-blog/"
   name "Turbo Boost Switcher Pro"
   desc "Enable and disable the Intel CPU Turbo Boost feature"
   homepage "https://www.rugarciap.com/turbo-boost-switcher-for-os-x/"
 
-  # App renamed to remove "(English)" suffix
+  suite "tbswitcher_resources"
   app "Turbo Boost Switcher Pro.app"
 
   uninstall quit: "rugarciap.com.Turbo-Boost-Switcher-Pro-Pro",
