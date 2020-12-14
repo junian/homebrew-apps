48c584b1251c12c80834a029320d3d77e9965ace
Junian Triajianto <author@junian.net>
2020-12-14 09:42:37 +0700

    Update Turbo Boost Switcher Pro

--- Casks/turbo-boost-switcher-pro.rb
+++ Casks/turbo-boost-switcher-pro.rb
@@ -2,9 +2,7 @@ cask "turbo-boost-switcher-pro" do
   version "2.10.2"
   sha256 "32ca4284914a6a69cd7dac0d8641a270323ba073a21be0ced044bb748691a9e4"
 
-  # files.gumroad.com/ was verified as official when first introduced to the cask
   url "https://files.gumroad.com/attachments/2401211130126/b5d96270c67544619bc6ccd1d5145142/original/Turbo_Boost_Switcher_Pro_v2.10.2.dmg?response-content-disposition=attachment&verify=#{ENV['HOMEBREW_GUMROADVERIFY']}"
-      verified: "files.gumroad.com/"
   appcast "https://www.rugarciap.com/turbo-boost-switcher-for-os-x-blog/"
   name "Turbo Boost Switcher Pro"
   desc "Enable and disable the Intel CPU Turbo Boost feature"
