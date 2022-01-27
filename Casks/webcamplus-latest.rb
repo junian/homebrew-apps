5c2d3d2e4e38f5b9bf791693739b65a2d9042f2f
Junian Triajianto <author@junian.net>
2022-01-27 16:08:58 +0700

    Webcamplus v1.2.0

--- Casks/webcamplus-latest.rb
+++ Casks/webcamplus-latest.rb
@@ -1,10 +1,10 @@
 cask "webcamplus-latest" do
 
-    version "1.0.3"
+    version "1.2.0"
 
     sha256 "8e4ba6a974bc2b6b8d3ec2c38631ab3dd9e00084ddd979b64e10a34b378c07e6"
     
-    url "https://github.com/junian/homebrew-apps/releases/download/latest/WebcamPlus.Installer.#{version}.pkg"
+    url "https://github.com/WebcamPlus/WebcamPlus.github.io/releases/download/#{version}/WebcamPlus.Installer.#{version}.pkg"
     name "Webcam Plus"
     desc "Use your iPhone as a webcam for your Mac"
     homepage "https://webcamplus.app"
