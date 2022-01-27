3333f7f2f3e50bf1216e6effbe5df6ec9168c614
Junian Triajianto <author@junian.net>
2022-01-27 16:10:03 +0700

    Fix sha sum

--- Casks/webcamplus-latest.rb
+++ Casks/webcamplus-latest.rb
@@ -2,7 +2,7 @@ cask "webcamplus-latest" do
 
     version "1.2.0"
 
-    sha256 "8e4ba6a974bc2b6b8d3ec2c38631ab3dd9e00084ddd979b64e10a34b378c07e6"
+    sha256 "50621515ff773b88930ffb2cfcd1511cf48cbf151672bd05bda37729b65ea509"
     
     url "https://github.com/WebcamPlus/WebcamPlus.github.io/releases/download/#{version}/WebcamPlus.Installer.#{version}.pkg"
     name "Webcam Plus"
