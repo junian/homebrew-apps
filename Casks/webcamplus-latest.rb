e1e16dcd6c071afdf5903e2e91c946931714e014
Junian Triajianto <author@junian.net>
2022-01-28 15:58:18 +0700

    Update webcamplus

--- Casks/webcamplus-latest.rb
+++ Casks/webcamplus-latest.rb
@@ -1,13 +1,16 @@
 cask "webcamplus-latest" do
+  version "1.2.0"
+  sha256 "50621515ff773b88930ffb2cfcd1511cf48cbf151672bd05bda37729b65ea509"
+  
+  url "https://github.com/WebcamPlus/WebcamPlus.github.io/releases/download/#{version}/WebcamPlus.Installer.#{version}.pkg"
+  name "Webcam Plus"
+  desc "Use your iPhone as a webcam"
+  homepage "https://webcamplus.app/"
 
-    version "1.2.0"
+  pkg "WebcamPlus.Installer.#{version}.pkg"
 
-    sha256 "50621515ff773b88930ffb2cfcd1511cf48cbf151672bd05bda37729b65ea509"
-    
-    url "https://github.com/WebcamPlus/WebcamPlus.github.io/releases/download/#{version}/WebcamPlus.Installer.#{version}.pkg"
-    name "Webcam Plus"
-    desc "Use your iPhone as a webcam for your Mac"
-    homepage "https://webcamplus.app"
-
-    pkg "WebcamPlus.Installer.#{version}.pkg"
+  uninstall pkgutil: [
+    "com.antonmeier.webcamplus.plugin",
+    "com.antonmeier.webcamplus.server",
+  ]
 end
