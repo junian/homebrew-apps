9cc45703d06fde0380d7195ad4f3c943afc61bea
Junian Triajianto <author@junian.net>
2022-02-03 21:12:33 +0700

    Fix webcam plus style

--- Casks/webcamplus-latest.rb
+++ Casks/webcamplus-latest.rb
@@ -1,7 +1,7 @@
 cask "webcamplus-latest" do
   version "1.2.0"
   sha256 "50621515ff773b88930ffb2cfcd1511cf48cbf151672bd05bda37729b65ea509"
-  
+
   url "https://github.com/WebcamPlus/WebcamPlus.github.io/releases/download/#{version}/WebcamPlus.Installer.#{version}.pkg"
   name "Webcam Plus"
   desc "Use your iPhone as a webcam"
