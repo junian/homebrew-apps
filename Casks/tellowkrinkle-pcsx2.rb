4e72cc0c60e4c89c132b9eb63a3847eb8ddf9b3c
Junian Triajianto <3881052+junian@users.noreply.github.com>
2022-03-26 22:00:31 +0700

    Update tellowkrinkle-pcsx2.rb

--- Casks/tellowkrinkle-pcsx2.rb
+++ Casks/tellowkrinkle-pcsx2.rb
@@ -1,6 +1,6 @@
 cask "tellowkrinkle-pcsx2" do
   version "1.7.0,20220325"
-  sha256 "f6d6eeb8eebce13effadcb6bb5cf6417fa502d4e67261bad791f21ffd14a72a6"
+  sha256 "6c5450718f88c6ff13613385fcac91e1c907aad707c3777463e74a2138eaf66e"
 
   url "https://github.com/tellowkrinkle/pcsx2/releases/download/macos-#{version.csv.second}/PCSX2-#{version.csv.first}-dev-#{version.csv.second}-Mac-64Bit.tar.gz"
   name "PCSX2"
