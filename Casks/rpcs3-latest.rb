e13392246a5a8ef8db556c3e45498d2628121b46
Junian Triajianto <3881052+junian@users.noreply.github.com>
2022-04-14 15:02:03 +0700

    Update rpcs3-latest.rb

--- Casks/rpcs3-latest.rb
+++ Casks/rpcs3-latest.rb
@@ -1,8 +1,8 @@
 cask "rpcs3-latest" do
-  version "0.0.21-13440,91768c025c25ef2fc17143f7b6604c1985d1d15c"
+  version "0.0.21-13441,d6b026314409980734605e9275950f835f04e8df"
   sha256 "d45bb759cab85278f9e6fcc60b860eef4ba34e642f504cd44ddbd33eefa64518"
 
-  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-91768c02_macos.dmg"
+  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-d6b02631_macos.dmg"
   name "RPCS3"
   desc "The Open-source PlayStation 3 Emulator"
   homepage "https://rpcs3.net/"
