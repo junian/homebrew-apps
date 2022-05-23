e07adf291370ca0516fae0791a61a4a359271757
Junian Triajianto <3881052+junian@users.noreply.github.com>
2022-05-23 15:24:23 +0700

    Update pdf-expert-app.rb

--- Casks/pdf-expert-app.rb
+++ Casks/pdf-expert-app.rb
@@ -8,8 +8,7 @@ cask "pdf-expert-app" do
   homepage "https://pdfexpert.com/"
 
   auto_updates true
-  conflicts_with cask: "homebrew/cask-versions/pdf-expert-beta",
-                       "pdf-expert"
+  conflicts_with cask: "pdf-expert"
   depends_on macos: ">= :high_sierra"
 
   app "PDF Expert.app"
