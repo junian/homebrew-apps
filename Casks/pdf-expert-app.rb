52fcf4b7d7fb2f90c628512128d260764fa984b2
Junian Triajianto <3881052+junian@users.noreply.github.com>
2022-05-23 15:20:54 +0700

    Update pdf-expert-app.rb

--- Casks/pdf-expert-app.rb
+++ Casks/pdf-expert-app.rb
@@ -2,13 +2,14 @@ cask "pdf-expert-app" do
   version "2.5.19"
   sha256 "c74924def94a828624ff332dc0b024792692cabe7a7f59799fd295928ba9a7a1"
 
-  url "http://192.168.2.4:8700/Apps/PDF.Expert.v#{version}.dmg"
+  url "http://192.168.1.4:8700/Apps/PDF.Expert.v#{version}.dmg"
   name "PDF Expert"
   desc "PDF reader, editor and annotator"
   homepage "https://pdfexpert.com/"
 
   auto_updates true
-  conflicts_with cask: "homebrew/cask-versions/pdf-expert-beta"
+  conflicts_with cask: "pdf-expert",
+                 cask: "homebrew/cask-versions/pdf-expert-beta"
   depends_on macos: ">= :high_sierra"
 
   app "PDF Expert.app"
