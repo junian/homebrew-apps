209449b8e06d3cccf76d44c262a239e955de84ca
Junian Triajianto <3881052+junian@users.noreply.github.com>
2022-08-23 13:08:23 +0700

    Update pd-runner-app.rb

--- Casks/pd-runner-app.rb
+++ Casks/pd-runner-app.rb
@@ -1,8 +1,8 @@
 cask "pd-runner-app" do
-  version "0.3.6"
-  sha256 "df9f9792034c689f881d45e0f0bd6c802b57a879779747302e59cbd37bfee673"
+  version "0.3.8"
+  sha256 "2c469438d167dd8fe1ae37f95dbe53e94bd1119bbf5eff008b01d8a75169f318"
 
-  url "http://192.168.1.4:8733/Apps/PD-Runner_v#{version}_Universal2.dmg"
+  url "http://192.168.1.4:8733/Apps/PD.Runner.#{version}_MacApp.dmg"
   name "PD Runner"
   desc "VM launcher for Parallels Desktop"
   homepage "https://github.com/lihaoyun6/PD-Runner/"
