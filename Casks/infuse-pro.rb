d2d9358b667f580087bff019c3e66d906a086531
Junian Triajianto <3881052+junian@users.noreply.github.com>
2022-11-22 17:38:01 +0700

    Update infuse-pro.rb

--- Casks/infuse-pro.rb
+++ Casks/infuse-pro.rb
@@ -1,8 +1,8 @@
 cask "infuse-pro" do
-  version "7.4.5"
-  sha256 "74ce46f919c5952cd075d23d49438f5fbc8b02f1f9bb90476eb1dc6138b506e1"
+  version "7.4.8"
+  sha256 "dc45eeddf9733d10bb8b811fca8e2c187714a351aafa9d1980c9abb22d8f79cf"
 
-  url "http://192.168.1.4:8733/Apps/Infuse.v#{version}.dmg"
+  url "http://192.168.1.4:8733/Apps/Infuse.PRO.v#{version}.dmg"
   name "Infuse 7"
   desc "Elegant video player"
   homepage "https://firecore.com/infuse/"
