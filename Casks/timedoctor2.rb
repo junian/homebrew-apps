e3c84ea0f15b76b5faa6162d260db1c3f2f7aeaf
Junian Triajianto <3881052+junian@users.noreply.github.com>
2023-11-24 19:07:12 +0700

    Update timedoctor2.rb

--- Casks/timedoctor2.rb
+++ Casks/timedoctor2.rb
@@ -1,6 +1,6 @@
 cask "timedoctor2" do
-  version "3.11.74"
-  sha256 "5ca5fc93321ed971f96986e1142e481afd8e80764bca1433bc04639c9a269193"
+  version "3.12.63"
+  sha256 "0b2cee423bba76fa81b74eff281a6a8d0a8461792f9be168a7712107ac499756"
   
   url "https://download.timedoctor.com/#{version}/macos/dmg/timedoctor2-#{version}-macos.dmg"
   name "Time Doctor 2"
