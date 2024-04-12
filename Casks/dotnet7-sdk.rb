1f43848f9235a522211f5d27bb1cc90556d90425
Junian Triajianto <3881052+junian@users.noreply.github.com>
2024-04-12 11:56:00 +0700

    Update dotnet7-sdk.rb

--- Casks/dotnet7-sdk.rb
+++ Casks/dotnet7-sdk.rb
@@ -2,12 +2,12 @@ cask "dotnet7-sdk" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "7.0.402,2054d7e3-f46c-4b44-9cb1-6b7f8418bc3d,bcb923c0a5d978f7590a176191931455"
-    sha256 "183b3a17754adc7e2906766ad4f62359e33a84f4e65fdbc85525dafd63578253"
+    version "7.0.408,5cd047f7-899a-4e00-9c86-d79fe7fe3d6e,82f1cb63ebebe92d084cfc7ab6204b6f"
+    sha256 "04dd3dab6265f299059e14aa87d6e26dd3207fb78fae6b8e768fdc789c9e6a80"
   end
   on_intel do
-    version "7.0.402,0998f773-80a6-4a6e-bf1f-4a83dd5df01f,ce12481071bbf6350a92e231c7217db6"
-    sha256 "b4d7b0b182e3bb7e3c9a5ab45ea6796035e0ee4534d033d9dadbf3c056dad3a2"
+    version "7.0.408,a54f7102-d834-48b0-b6f4-32d9d215dd51,57c2418d3d1d7be290e032e535af2d13"
+    sha256 "ca3e72f9dab83dba6194bff886711483cb3c590cf9c6bbcdf8d0dbb6e1622bfd"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
