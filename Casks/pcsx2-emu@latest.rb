bf27837c101b302255a73e4abf6b776764ea9e66
Junian Triajianto <3881052+junian@users.noreply.github.com>
2024-04-30 08:47:31 +0700

    Update and rename pcsx2-emu.rb to pcsx2-emu@latest.rb

--- Casks/pcsx2-emu@latest.rb
+++ Casks/pcsx2-emu@latest.rb
@@ -1,11 +1,11 @@
-cask "pcsx2-emu" do
-  version "1.7.2755,7c9493c"
-  sha256 "d3b6461246d360eff2fc1bb7f1ad70c8b151f3cd20b88e2b8d6a141cf60b0888"
+cask "pcsx2-emu@latest" do
+  version "1.7.5760"
+  sha256 "ed34ad3d0f58173050323bc769fb498f993f6a56d9004d0ec070e9572bb91bf3"
 
   url "https://github.com/PCSX2/pcsx2/releases/download/v#{version.csv.first}/pcsx2-v#{version.csv.first}-macOS.tar.gz"
   name "PCSX2"
   desc "Open-Source Playstation 2 Emulator Supporting Over 98% Of The Original PS2 Library"
   homepage "https://pcsx2.net/"
 
-  app "PCSX2-sha[#{version.csv.second}].app", target: "PCSX2.app"
+  app "PCSX2-v#{version.csv.first}.app", target: "PCSX2.app"
 end
