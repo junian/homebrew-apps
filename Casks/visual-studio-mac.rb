29816b985873b6981ead0609171367c95cff428a
Junian Triajianto <3881052+junian@users.noreply.github.com>
2024-05-04 10:42:49 +0700

    Update and rename visual-studio-mac-preview.rb to visual-studio-mac.rb

---
+++ Casks/visual-studio-mac.rb
@@ -0,0 +1,53 @@
+cask "visual-studio-mac" do
+  arch arm: "arm64", intel: "x64"
+
+  on_arm do
+    version "17.6.11.400,d9bdf235-5f23-4901-a4d4-88b1fc208e50,4d6cb7eba0d59cb70f7ef2adaf48fc60"
+    sha256 "1a6ace31b0c05d09e3b4eb4fa8cd99596518aaaae573d9a24886ecd4691be1af"
+  end
+  on_intel do
+    version "17.6.11.400,d9bdf235-5f23-4901-a4d4-88b1fc208e50,5afb005d0e71c07fcfbd518d1b0de9e0"
+    sha256 "460f9a407b34790dda5bc9f9d4470d1f7dd80dec8f3f7dc672a3ebda07c26a93"
+  end
+
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/visualstudioformac-#{version.csv.first}-#{arch}.dmg"
+  name "Microsoft Visual Studio"
+  desc "Integrated development environment"
+  homepage "https://visualstudio.microsoft.com/vs/mac/"
+
+  livecheck do
+    url "https://aka.ms/vsmac/manifest/#{version.major}-stable"
+    regex(%r{/download/pr/([^/]+)/([^/]+)/visualstudioformac[._-]v?(\d+(?:\.\d+)+)-#{arch}.dmg}i)
+    strategy :json do |json|
+      json["items"].flat_map do |item|
+        item["url"]&.scan(regex)&.map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+  depends_on cask: "mono-mdk-for-visual-studio"
+
+  app "Visual Studio.app"
+
+  zap trash: [
+    "/Applications/Xamarin Profiler.app",
+    "/Applications/Xamarin Workbooks.app",
+    "~/Library/Application Support/CrashReporter/VisualStudio*",
+    "~/Library/Application Support/VisualStudio",
+    "~/Library/Caches/VisualStudio",
+    "~/Library/Caches/VisualStudioInstaller",
+    "~/Library/Caches/XamarinInstaller",
+    "~/Library/Developer/Xamarin",
+    "~/Library/Logs/VisualStudio",
+    "~/Library/Logs/VisualStudioInstaller",
+    "~/Library/Logs/XamarinInstaller",
+    "~/Library/MonoAndroid",
+    "~/Library/MonoTouch",
+    "~/Library/Preferences/Visual*Studio",
+    "~/Library/Preferences/Xamarin",
+    "~/Library/VisualStudio",
+    "~/Library/Xamarin.Mac",
+  ]
+end
